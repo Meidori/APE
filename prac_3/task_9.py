b82a04c45ea549ee77b1a931cc854cf46772556b
meidori <meidorislav@gmail.com>
2024-11-16 22:53:44 +0300

    prac_3 - task_9 - Addition to the previous commit: the program processes only the first 25 movies from the list instead of 250.

--- prac_3/task_9.py
+++ prac_3/task_9.py
@@ -4,8 +4,13 @@ import random
 
 
 def get_description(link):
+    # делает отдельный запрос на каждую страницу фильма, чтобы получить описание
+    # эта функция вызывается в цикле 10 раз
+    # это 10 дополнительных запросов
+    # Запросы занимают значительное время - поэтому программа выполняется максимально медленно
     base_url = "https://www.imdb.com"
     url = base_url + link
+    # Чтобы не было 403 Forbidden
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
@@ -34,7 +39,8 @@ def get_random_top_10_movies():
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             soup = bs(response.text, "html.parser")
-            links = [a["href"] for a in soup.find_all('a', class_="ipc-title-link-wrapper", href=True)]
+            # Неоптимальная работа с данными, поэтому не включает все 250 фильмов со странице
+            links = [a["href"] for a in soup.find_all('a', class_="ipc-title-link-wrapper", href=True)]                                 
             titles = [a.find('h3').get_text().strip() for a in soup.find_all('a', class_="ipc-title-link-wrapper") if a.find('h3')]
             
             recs = dict()
@@ -60,6 +66,8 @@ def get_random_top_10_movies():
 
 
 def main():
+    # Программа работает максимально медленно
+    # + она берет только первые 25 фильмов, а не 250
     movies = get_random_top_10_movies()
     for k, v in movies.items():
         print("--------------------")
